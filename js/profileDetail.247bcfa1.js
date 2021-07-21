bae95191c7617b2c1ad64f9537e056676239807f
winaki802 <60755932+winaki802@users.noreply.github.com>
2021-07-21 19:28:17 +0900

    Delete js directory

--- js/profileDetail.247bcfa1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["profileDetail"],{ead7:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},i=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"Home"},[n("h1",[e._v("This is an ProfileDetail page ")])])}],l=n("2877"),s={},r=Object(l["a"])(s,a,i,!1,null,null,null);t["default"]=r.exports}}]);
-//# sourceMappingURL=profileDetail.247bcfa1.js.map
